6bb50b1aee0143dfbb94c5be43a207018293fa16
杨小事er <Uiryzd@163.com>
2018-03-01 19:37:44 +0800

    fix(*)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -51,42 +51,39 @@ export default class App extends React.Component {
     const sidebar = (<List>
       {sidebarArr.map((i, index) => {
         if (index === 0) {
-          return (<List.Item key={index} 
-            // thumb={i.image}
-            multipleLine
-          >
-            <Link to="/s1" onClick={() => {
-              this.setState({ open: false })
-            }}>
-              <div style={{textAlign: 'center'}}>
-                <img src={i.image} style={{width:'8rem', height: '8rem'}}/>
-                <div style={{backgroundColor:'red'}}>{i.content}</div>
-              </div>
-              {/*登录注册，登录后头像*/}
-
+          return (
+            <List.Item key={index} multipleLine>
+              <Link to="/s1" onClick={() => { this.setState({ open: false }) }}>
+                <div style={{textAlign: 'center'}}>
+                  <img src={i.image} style={{width:'8rem', height: '8rem'}}/>
+                  <div style={{backgroundColor:'red'}}>{i.content}</div>
+                </div>
+                {/*登录注册，登录后头像*/}
+              </Link>
+          </List.Item>
+          );
+        }
+        return (
+          <List.Item key={index} thumb={i.image}>
+            <Link to={"/s"+(index+1)} onClick={() => { this.setState({ open: false })}}>
+              {i.content}
             </Link>
           </List.Item>);
-        }
-        return (<List.Item key={index}
-          thumb={i.image}
-        ><Link to={"/s"+(index+1)} onClick={() => {
-            this.setState({ open: false })
-        }}>{i.content}</Link></List.Item>);
       })}
     </List>);
 
     const buttonStyle = { marginRight: '16px' };
-    const homeButton = path =='/' ? null :<Icon key="0" type="search" style={buttonStyle} onClick={() => hashHistory.push('/')}/>;
+    const homeButton = path === '/' ? null :<Icon key="0" type="search" style={buttonStyle} onClick={() => hashHistory.push('/')}/>;
     const drawerDiv = { position: 'relative', height: 'calc(100% - 45px)' };
     const sidebarStyle = { backgroundColor: '#fff' };
     return (
       <div className="container">
         <NavBar mode="light"
-          icon={path =='/' ? null : <Icon type="left" /> }
+          icon={path === '/' ? null : <Icon type="left" /> }
           onLeftClick={() => hashHistory.goBack()}
           rightContent={[homeButton, <b onClick={() => this.setState({ open: !this.state.open })}>...</b>]}
         >
-          {path =='/' ? 'CS:GO系列赛' : this.state.title}
+          {path === '/' ? 'CS:GO系列赛' : this.state.title}
         </NavBar>
 
         <div style={drawerDiv}>
---
+++ src/reducer.js
@@ -0,0 +1,5 @@
+import {combineReducers} from 'redux'
+
+
+// 合并所有reducers 并且返回
+export default combineReducers({})
